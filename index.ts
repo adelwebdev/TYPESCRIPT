da5bacea0d75792d1dc1ae270688571957a71d0d
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-05-28 00:22:15 +0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log("typescript code");
+
+let tab = ["chien", "chat", "poisson"];
+
+console.log(tab);
---
+++ test.js
@@ -0,0 +1,6 @@
+let num = 24;
+
+num = "lol";
+console.log(num);
+
+console.log("typescript code");
