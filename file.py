8351e6df5b236904cd6bc338cbec6dc31ce7918c
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-06-05 21:08:02 +0300

    Delete file.py

--- file.py
+++
@@ -1,6 +0,0 @@
-ageDeEricPraline      = 50
-ageDeHenryWensleydale = 20
-
-# Test
-if 0 > ageDeEricPraline > 150 or ageDeEricPraline > ageDeHenryWensleydale:
-    print("l'age de Eric Praline est incorrect")
